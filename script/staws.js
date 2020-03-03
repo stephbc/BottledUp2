63bcf0abfc83dde40d30e1deca1163fe090675f1
Jennifer Nugent <jmn2158@columbia.edu>
2020-03-03 16:04:49 -0500

    created straws array

---
+++ script/staws.js
@@ -0,0 +1,74 @@
+const straws = [
+  {
+    name: 'Through the Drinking Glass',
+    type: 'straws',
+    material: 'Glass',
+    price: 25.0,
+    inventory: 100,
+    color: 'Clear',
+    imgUrl:
+      'https://cb2.scene7.com/is/image/CB2/GlassStrawROF16/?$web_zoom$&190905021622&wid=450&hei=450',
+    description:
+      'These handblown glass straws will make you feel like you have entered a wonderland of beverage options. With their ergonomic shape, making an eco-friendly choice becomes easy, even elegant! The double-walled glass and crystal-clear finish makes drinking colorful smoothies a treat, without sacrificing durability./n Product Specifications: Set of 5 bent, 8 inch glass straws with cleaning brush'
+  },
+  {
+    name: 'Highly Polished',
+    type: 'straws',
+    material: 'Stainless Steel',
+    price: 25.0,
+    inventory: 100,
+    color: 'Rose Gold',
+    imgUrl:
+      'https://i.etsystatic.com/17722707/r/il/c4e514/1655935591/il_1588xN.1655935591_j0l8.jpg',
+    description:
+      'These stainless steel straws are the perfect accessory to any glassware! Each set is an ever-lasting stainless steel with your choice of colored metal finish. Unlike the one-tone stainless steel of old, no longer will you need to sacrifice a uniforn metail look in your home in service of your commitment to sustainability. Guests will know immediately that you are a polished individual who has a carefully curated cupboard that leaves no detail overlooked./n Product Specifications: Set of 5 bent, 8 inch straws with cleaning brush'
+  },
+  {
+    name: 'Highly Polished',
+    type: 'straws',
+    material: 'Stainless Steel',
+    price: 25.0,
+    inventory: 100,
+    color: 'Gold',
+    imgUrl:
+      'https://i.etsystatic.com/17722707/r/il/70c151/1655923555/il_1588xN.1655923555_fh3l.jpg',
+    description:
+      'These stainless steel straws are the perfect accessory to any glassware! Each set is an ever-lasting stainless steel with your choice of colored metal finish. Unlike the one-tone stainless steel of old, no longer will you need to sacrifice a uniforn metail look in your home in service of your commitment to sustainability. Guests will know immediately that you are a polished individual who has a carefully curated cupboard that leaves no detail overlooked./n Product Specifications: Set of 5 bent, 8 inch straws with cleaning brush'
+  },
+  {
+    name: 'Highly Polished',
+    type: 'straws',
+    material: 'Stainless Steel',
+    price: 25.0,
+    inventory: 100,
+    color: 'Platinum',
+    imgUrl:
+      'https://i.etsystatic.com/17722707/r/il/1519f6/1931565309/il_1588xN.1931565309_2jjc.jpg',
+    description:
+      'These stainless steel straws are the perfect accessory to any glassware! Each set is an ever-lasting stainless steel with your choice of colored metal finish. Unlike the one-tone stainless steel of old, no longer will you need to sacrifice a uniforn metail look in your home in service of your commitment to sustainability. Guests will know immediately that you are a polished individual who has a carefully curated cupboard that leaves no detail overlooked./n Product Specifications: Set of 5 bent, 8 inch straws with cleaning brush'
+  },
+  {
+    name: 'Highly Polished',
+    type: 'straws',
+    material: 'Stainless Steel',
+    price: 25.0,
+    inventory: 100,
+    color: 'Pitch Black',
+    imgUrl:
+      'https://i.etsystatic.com/17722707/r/il/85707b/1931560701/il_1588xN.1931560701_s494.jpg',
+    description:
+      'These stainless steel straws are the perfect accessory to any glassware! Each set is an ever-lasting stainless steel with your choice of colored metal finish. Unlike the one-tone stainless steel of old, no longer will you need to sacrifice a uniforn metail look in your home in service of your commitment to sustainability. Guests will know immediately that you are a polished individual who has a carefully curated cupboard that leaves no detail overlooked./n Product Specifications: Set of 5 bent, 8 inch straws with cleaning brush'
+  },
+  {
+    name: 'Dàxióngmāo',
+    type: 'straws',
+    material: 'Bamboo',
+    price: 25.0,
+    inventory: 100,
+    color: 'Natural Wood',
+    imgUrl:
+      'https://reusablesuperstore.com/wp-content/uploads/2019/03/Reusable-bamboo-straws-UK.jpg',
+    description:
+      'These bamboo straws have a calming, natural look and their shape makes them perfect to use at home or tuck into a bag to take with you anywhere! Bamboo grows quickly while consuming fewer resources than any other type of wood, so these straws pack an even greater environmental punch! This product is made even more special because it is named after the endangered giant panda. With each sale, BottledUp will donate $1.00 to the World Wildlife Fund. Make a purchase and a difference with the click of a button. /n Product Specifications: Set of 5 straight, 8 inch straws with cleaning brush'
+  }
+]
