d99d97c0b24060202cabc65c32f797bb9dbb8681
Jane An <janeyuhwian@gmail.com>
2020-03-03 12:55:54 -0500

    updated bottles

--- script/seed.js
+++ script/seed.js
@@ -51,7 +51,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Iridescent',
       imgUrl: 'https://s7d5.scene7.com/is/image/UrbanOutfitters/49328438_017_b?$xlarge$&hei=900&qlt=80&fit=constrain',
-      description: 'The Sarah is a refined bottle for the go-getter. This is a reusable bottle that is no longer relegated to hiking or yoga, the Sarah can be brought into work or can join you at corporate events and galas. With this bottle, your Perrier will stay fresh for the entirety of the ride to your weekend home in the Hamptons./n Product Specifications: 16oz’
+      description: 'The Sarah is a refined bottle for the go-getter. This is a reusable bottle that is no longer relegated to hiking or yoga, the Sarah can be brought into work or can join you at corporate events and galas. With this bottle, your Perrier will stay fresh for the entirety of the ride to your weekend home in the Hamptons./n Product Specifications: 16oz',
      },
      {
       name: 'The Sarah',
@@ -61,7 +61,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Fresh Herbs',
       imgUrl: 'https://s7d5.scene7.com/is/image/UrbanOutfitters/49328438_030_f?$xlarge$&hei=900&qlt=80&fit=constrain',
-      description: 'The Sarah is a refined bottle for the go-getter. This is a reusable bottle that is no longer relegated to hiking or yoga, the Sarah can be brought into work or can join you at corporate events and galas. With this bottle, your Perrier will stay fresh for the entirety of the ride to your weekend home in the Hamptons./n Product Specifications: 16oz’
+      description: 'The Sarah is a refined bottle for the go-getter. This is a reusable bottle that is no longer relegated to hiking or yoga, the Sarah can be brought into work or can join you at corporate events and galas. With this bottle, your Perrier will stay fresh for the entirety of the ride to your weekend home in the Hamptons./n Product Specifications: 16oz',
      },
      {
       name: 'The Sarah',
@@ -71,7 +71,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Pressed Florals',
       imgUrl: 'https://s7d5.scene7.com/is/image/UrbanOutfitters/49328438_095_d?$xlarge$&hei=900&qlt=80&fit=constrain',
-      description: 'The Sarah is a refined bottle for the go-getter. This is a reusable bottle that is no longer relegated to hiking or yoga, the Sarah can be brought into work or can join you at corporate events and galas. With this bottle, your Perrier will stay fresh for the entirety of the ride to your weekend home in the Hamptons./n Product Specifications: 16oz’
+      description: 'The Sarah is a refined bottle for the go-getter. This is a reusable bottle that is no longer relegated to hiking or yoga, the Sarah can be brought into work or can join you at corporate events and galas. With this bottle, your Perrier will stay fresh for the entirety of the ride to your weekend home in the Hamptons./n Product Specifications: 16oz',
      },
      {
       name: 'The David',
@@ -81,7 +81,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Noir',
       imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/71RGWazGppL._SL1500_.jpg',
-      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a starry night finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz’
+      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a starry night finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz',
      },
      {
       name: 'The David',
@@ -91,7 +91,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Bleu',
       imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/61JYlm2sVcL._SL1350_.jpg',
-      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a deep navy finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz’
+      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a deep navy finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz',
      },
      {
       name: 'The David',
@@ -101,7 +101,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Turquoiss',
       imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/31DNRXHQOkL._SL1000_.jpg',
-      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a bright, turquoise finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz’
+      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a bright, turquoise finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz',
      },
      {
       name: 'The David',
@@ -111,7 +111,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Vert',
       imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/51t%2BnZsOw8L._SL1259_.jpg',
-      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a forest green finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz’
+      description: 'The David is an ideal way to start your new eco-friendly life. The bottle is designed to be lightweight with a forest green finish, an understated accompaniment for your weekend strolls or morning Equinox sessions./n Product Specifications: 24oz',
      },
      {
       name: 'The Bienvenido',
@@ -121,7 +121,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Sunny Disposition',
       imgUrl: 'https://www.hydroflask.com/media/catalog/product/cache/1/image/600x/9df78eab33525d08d6e5fb8d27136e95/w/2/w20ts2-sunflower.jpg',
-      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz’
+      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz',
      },
      {
       name: 'The Bienvenido',
@@ -131,7 +131,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Seaside',
       imgUrl: 'https://www.hydroflask.com/media/catalog/product/cache/1/image/600x/9df78eab33525d08d6e5fb8d27136e95/w/2/w20ts2-pacific_1.jpg',
-      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz’
+      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz',
      },
      {
       name: 'The Bienvenido',
@@ -141,7 +141,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Central Park',
       imgUrl: 'https://www.hydroflask.com/media/catalog/product/cache/1/image/600x/9df78eab33525d08d6e5fb8d27136e95/w/2/w20ts2-olive.jpg',
-      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz’
+      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz',
      },
      {
       name: 'The Bienvenido',
@@ -151,7 +151,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Bismol',
       imgUrl: 'https://www.hydroflask.com/media/catalog/product/cache/1/image/600x/9df78eab33525d08d6e5fb8d27136e95/w/2/w20ts2-watermelon.jpg',
-      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz'
+      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz',
      },
      {
       name: 'The Bienvenido',
@@ -161,7 +161,7 @@ const seed = async () => {
       inventory: 100,
       color: 'Business Attire',
       imgUrl: 'https://www.hydroflask.com/media/catalog/product/cache/1/image/600x/9df78eab33525d08d6e5fb8d27136e95/w/2/w20ts2-black.jpg',
-      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz’
+      description: 'The Bienvenido will keep you fresh on a Summer day and warm on a winter night. With double-walled stainless steel, your drinks will be kept cold for 24h ours or warm for 12 hours, allowing you to say Welcome to a new way of enjoying your favorite beverages./n Product Specifications: 20oz',
      }]
      
     await Promise.all(users.map((user) => {
