00d2d934263f177151d880643257390f2c7ade70
Rachel Burrous <rachelashleyburrous@gmail.com>
2020-03-05 16:30:42 -0500

    using isloggedin to toggle form

--- client/components/UserAccount.js
+++ client/components/UserAccount.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {me} from '../store'
 
-export const UserAccount = props => {
-  console.log(props)
-  const {user} = props
+export const UserAccount = ({props, isLoggedIn}) => {
+  // console.log(props)
+  // const {user} = props
   return (
     <div>
-      <p>
+      {isLoggedIn ? (
+        <div>
+          {/* <p>
         Name: {user.firstName} {user.lastName}
       </p>
       <p>Email: {user.email}</p>
       <p>Address: {user.address}</p>
-      <p>Account Type: {user.accountType}</p>
-      <button type="button">Change Password</button>
+      <p>Account Type: {user.accountType}</p> */}
+          <button type="button">Change Password</button>
+        </div>
+      ) : (
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input name="name" />
+          <label htmlFor="email">Email:</label>
+          <input name="email" />
+          <label htmlFor="address">Address:</label>
+          <input name="address" />
+          <label htmlFor="accountType">Account Type:</label>
+          <input name="accountType" />
+        </div>
+      )}
     </div>
   )
 }
 
 const mapState = state => {
   return {
-    user: state.user
+    user: state.user,
+    isLoggedIn: !!state.user.id
   }
 }
 
-export default connect(mapState)(UserAccount)
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData() {
+      dispatch(me())
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(UserAccount)
