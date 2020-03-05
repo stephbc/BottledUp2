d85b69194f1e3ec78e6644cf780c0c0aab1c6655
StephC <stephaniebchiang@gmail.com>
2020-03-05 15:14:30 -0500

    still broken

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -10,7 +10,7 @@ class AllProducts extends React.Component {
 
   render() {
     const productArray = this.props.products
-
+    console.log(this.state)
     if (!productArray) {
       return (
         <div>
