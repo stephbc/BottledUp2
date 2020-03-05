1f906ce5f73eb2260640a0906ce60ce9c3b032f3
Rachel Burrous <rachelashleyburrous@gmail.com>
2020-03-05 16:49:02 -0500

    yay loginandsignup fixed

--- client/components/UserAccount.js
+++ client/components/UserAccount.js
@@ -2,19 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {me} from '../store'
 
-export const UserAccount = ({props, isLoggedIn}) => {
-  // console.log(props)
-  // const {user} = props
+export const UserAccount = props => {
+  console.log(props)
+  const {user} = props
   return (
     <div>
-      {isLoggedIn ? (
+      {props.isLoggedIn ? (
         <div>
-          {/* <p>
-        Name: {user.firstName} {user.lastName}
-      </p>
-      <p>Email: {user.email}</p>
-      <p>Address: {user.address}</p>
-      <p>Account Type: {user.accountType}</p> */}
+          <p>
+            Name: {user.firstName} {user.lastName}
+          </p>
+          <p>Email: {user.email}</p>
+          <p>Address: {user.address}</p>
+          <p>Account Type: {user.accountType}</p>
           <button type="button">Change Password</button>
         </div>
       ) : (
