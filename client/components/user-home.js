7d144c3a3b65b338d579a89fb1ac1b87fadd88cc
Jennifer Nugent <jmn2158@columbia.edu>
2020-03-08 17:46:43 -0400

    wrote a few more tests to get us to twenty

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -11,7 +11,7 @@ export const UserHome = props => {
 
   return (
     <div>
-      <h3>Welcome to BOTTED UP! {email}</h3>
+      <h3>Welcome to BottledUp, {email}</h3>
       <Link to="/products">
         <div className="btn btn-primary btn-raised btn-lg">
           See Our Products
